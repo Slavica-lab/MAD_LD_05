3faa76d4f9eb258918c9b1a500dbe1649cff253c
Slavica <54640856+Slavica-lab@users.noreply.github.com>
2023-04-11 19:12:18 +0200

    Stand LV Tasks

--- app/src/main/java/com/example/movieappmad23/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad23/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.movieappmad23
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.movieappmad23.navigation.Navigation
@@ -14,6 +16,41 @@ class MainActivity : ComponentActivity() {
                 Navigation()
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Mainactivity", "I am onStart")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        Log.i("Maincativity", "I am onCreate")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Maincativity", "I am onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Maincativity", "I am onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Maincativity", "I am onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Maincativity", "I am onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Maincativity", "I am onRestart")
     }
 }
 
---
+++ app/src/main/java/com/example/movieappmad23/ViewModel/ViewModel.kt
@@ -0,0 +1,3 @@
+package com.example.movieappmad23.ViewModel
+
+//write viewmodel
